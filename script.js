7d55bd1cdf27c719afc88a202a386921eb42d26f
aditya-niranjan <adityaniranjan57@gmail.com>
2025-11-23 17:31:06 +0530

    added the API-BASE-URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = window.location.origin;
+const API_BASE_URL = 'https://smart-summarizer-1-njog.onrender.com';
 
 // DOM Elements
 let currentTab = 'youtube';
